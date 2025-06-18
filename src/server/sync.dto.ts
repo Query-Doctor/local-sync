f50c45b91803215e263c92d9d782b9bf948d9950
Chris Harris <christopherharris2012@gmail.com>
2025-06-18 09:51:52 -0500

    fix: allow valid postgresql prefix in conn string

--- src/server/sync.dto.ts
+++ src/server/sync.dto.ts
@@ -1,7 +1,15 @@
 import { z } from "zod/v4";
 
 export const SyncRequest = z.object({
-  db: z.string().startsWith("postgres://"),
+  // We shouldn't be doing separate validations on both the front end and here? Should probably consolidate
+  db: z
+    .string()
+    .refine(
+      (val) => val.startsWith("postgres://") || val.startsWith("postgresql://"),
+      {
+        message: "Must start with 'postgres://' or 'postgresql://'",
+      }
+    ),
   seed: z.coerce.number().min(0).max(1).default(0),
   schema: z.coerce.string().default("public"),
   requiredRows: z.coerce.number().positive().default(2),
